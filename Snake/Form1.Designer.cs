fb3d5bd337c8001dbd21329a97d5374d2d6e0945
tbalint11 <79862208+tbalint11@users.noreply.github.com>
2021-11-14 21:23:00 +0100

    Add files via upload

--- Snake/Form1.Designer.cs
+++ Snake/Form1.Designer.cs
@@ -43,15 +43,12 @@ namespace snake
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackgroundImage = global::kigyosjatek.Properties.Resources.hatter;
             this.ClientSize = new System.Drawing.Size(584, 561);
-            this.ForeColor = System.Drawing.SystemColors.Desktop;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Snake";
-            this.ShowIcon = false;
-            this.ShowInTaskbar = false;
             this.Text = "Snake";
+
             this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Snake_KeyDown);
             this.ResumeLayout(false);
 
